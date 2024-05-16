2e30e509dde7e61e4d2c2f312f8a7c0f66d173ad
Tim Perry <pimterry@gmail.com>
2024-05-16 18:26:25 +0200

    Add a placeholder TS source, test & CI setup

---
+++ test/test.spec.ts
@@ -0,0 +1,11 @@
+import { expect } from "chai";
+
+import { PLACEHOLDER } from "../src/index";
+
+describe("Usbmux-client", () => {
+
+    it("placeholder test", () => {
+        expect(PLACEHOLDER).to.equal(true);
+    });
+
+});
