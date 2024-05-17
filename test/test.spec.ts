c74582df1765d0ed2ef4801406184c0bf354a33c
Tim Perry <pimterry@gmail.com>
2024-05-17 18:11:06 +0200

    WIP: connect to usbmux and scan for devices

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,11 +1,27 @@
 import { expect } from "chai";
+import { delay } from "@httptoolkit/util";
 
-import { PLACEHOLDER } from "../src/index";
+import { UsbmuxClient, getUsbmuxClient } from "../src/index";
 
 describe("Usbmux-client", () => {
 
-    it("placeholder test", () => {
-        expect(PLACEHOLDER).to.equal(true);
+    let client: UsbmuxClient | undefined;
+
+    afterEach(() => {
+        client?.close();
+        client = undefined;
+    })
+
+    it("can expose the connected devices ", async () => {
+        client = await getUsbmuxClient();
+        await delay(10); // Not clear this is necessary, but not unhelpful
+
+        const devices = client.getDevices();
+
+        // Tests assume no devices (since this will always be the state in
+        // the CI environment) but this at least confirms we can connect
+        // to Usbmux successfully.
+        expect(devices).to.deep.equal({});
     });
 
 });
